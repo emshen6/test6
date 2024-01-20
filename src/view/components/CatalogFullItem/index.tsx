dce27d03ed3c6ae148611d4f041bbed98e100fbb
emshen6 <312637@niuitmo.ru>
2024-01-20 06:34:15 +0300

    CatalogFullItem

---
+++ src/view/components/CatalogFullItem/index.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import classNames from 'classnames';
+import { ru } from './i18n/ru.ts';
+import styles from './index.module.scss';
+
+interface IProductDetailsProps {
+    productName: string;
+    description: string;
+    img: string;
+}
+const cx = classNames.bind(styles);
+
+export const CatalogFullItem: React.FC<IProductDetailsProps> = (props) => {
+    return (
+        <div className={cx(styles.fullItemContainer)}>
+            <img loading="lazy" srcSet={props.img} className={cx(styles.fullItemImage)} />
+            <span className={cx(styles.catalogFullItem)}>
+                <div className="flex items-stretch justify-between gap-5">
+                    <span className="flex flex-col items-stretch justify-center">
+                        <div className={cx(styles.productTitle)}>{props.productName}</div>
+                    </span>
+                    <button className={cx(styles.toCartButton)}>{ru.to_cart}</button>
+                </div>
+                <div className={cx(styles.descriptionTitle)}>{ru.description}</div>
+                <div className={cx(styles.description)}>{props.description}</div>
+            </span>
+        </div>
+    );
+};
