89ddb8a33452c26e1bafad21518962cd35c79cb2
emshen6 <312637@niuitmo.ru>
2024-01-26 09:35:42 +0300

    make redux store persistent

--- src/core/store/index.ts
+++ src/core/store/index.ts
@@ -1,14 +1,28 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { combineReducers, configureStore } from '@reduxjs/toolkit';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 import { productApi } from '../api/product';
 import cart from '../api/cart/index';
+import form from '../api/form';
+
+const persistConfig = {
+    key: 'root',
+    storage,
+};
+
+const rootReducer = combineReducers({
+    [productApi.reducerPath]: productApi.reducer,
+    cart,
+    form,
+});
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 export const store = configureStore({
-    reducer: {
-        [productApi.reducerPath]: productApi.reducer,
-        cart,
-    },
+    reducer: persistedReducer,
     middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(productApi.middleware),
 });
 
+export const persistor = persistStore(store);
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,14 @@ import ReactDOM from 'react-dom/client';
 import './index.scss';
 import { RouterProvider } from 'react-router-dom';
 import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
 import { router } from './router';
-import { store } from './core/store';
+import { store, persistor } from './core/store';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <Provider store={store}>
-        <RouterProvider router={router} />
+        <PersistGate loading={null} persistor={persistor}>
+            <RouterProvider router={router} />
+        </PersistGate>
     </Provider>,
 );
