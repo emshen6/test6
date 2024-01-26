d978aa8a6720b3ce6da3d2f79062a0fe485e6f20
dustov2003 <dustov2003@gmail.com>
2024-01-26 21:30:56 +0300

    CatalogPage

---
+++ src/tests/CatalogPage.test.js
@@ -0,0 +1,21 @@
+const puppeteer = require('puppeteer');
+
+describe('catalog page', () => {
+    let browser;
+    let page;
+    beforeAll(async () => {
+        browser = await puppeteer.launch({headless: 'new'})
+        page = await browser.newPage()
+    })
+
+    afterAll(async () => {
+        await browser.close()
+    })
+
+    test('page loads correctly', async() => {
+        await page.goto('http://localhost:5173/catalog');
+        await page.waitForSelector('#title')
+        const html = await page.$eval('#title', e => e.innerHTML);
+        expect(html).toBe('Каталог');
+    }, 10000)
+})
