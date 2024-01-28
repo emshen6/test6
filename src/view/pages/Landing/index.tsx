2da1121962f271d9202637f97df8789571693067
emshen6 <312637@niuitmo.ru>
2024-01-28 15:05:37 +0300

    fix

--- src/view/pages/Landing/index.tsx
+++ src/view/pages/Landing/index.tsx
@@ -1,13 +1,10 @@
 import React from 'react';
 import classNames from 'classnames';
 import styles from './index.module.scss';
-import Icon from '../../components/UI/Icon';
-import { ICON } from '../../components/UI/Icon/IconType.tsx';
-import { Footer } from '../../components/Footer/index.tsx';
-import { useGetProductsQuery } from '../../../core/api/product';
 import { FactoryImage } from '../../../assets/image/FactoryImage';
 import { Header } from '../../components/Header';
-import { Benefits } from '../../components/Benefits/index.tsx';
+import { Benefits } from '../../components/Benefits';
+import { Footer } from '../../components/Footer';
 import { About } from '../../components/About';
 import { CatalogPart } from '../../components/CatalogPart';
 
